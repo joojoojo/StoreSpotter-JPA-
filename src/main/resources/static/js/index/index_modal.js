8c93c6a6fa5d052104d284542a526559608750af
주용현 <say06151@naver.com>
2024-09-09 01:01:07 +0900

    main-modal-add.v16

--- src/main/resources/static/js/index/index_modal.js
+++ src/main/resources/static/js/index/index_modal.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    // 페이지 로드 시 쿠키 확인하여 모달 표시/숨김
+    var checkCookie = getCookie("mycookie");
+    if (checkCookie !== 'popupEnd') {
+        $(".info-modal").css("display", "block");
+    }
+
     // "닫기" 버튼 클릭 시 모달 닫기
     $(".close_btn").click(function() {
         $(".info-modal").css("display", "none");
@@ -9,14 +15,6 @@ $(document).ready(function() {
         $(".info-modal").css("display", "none");
         setCookie("mycookie", 'popupEnd', 1); // 1일 후 만료되는 쿠키 설정
     });
-
-    // 페이지 로드 시 쿠키 확인하여 모달 표시/숨김
-    var checkCookie = getCookie("mycookie");
-    if (checkCookie === 'popupEnd') {
-        $(".info-modal").css("display", "none");
-    } else {
-        $(".info-modal").css("display", "block");
-    }
 });
 
 // 쿠키 설정 함수
